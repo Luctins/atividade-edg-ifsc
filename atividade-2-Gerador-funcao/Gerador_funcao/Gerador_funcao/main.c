c5574822e95e6a2f4ccf6f958862ff4acc6ff80d
Willemann <m.r.willemann@gmail.com>
2020-08-30 16:02:10 -0300

    Cria projeto 2 no Atmel

---
+++ atividade-2-Gerador-funcao/Gerador_funcao/Gerador_funcao/main.c
@@ -0,0 +1,18 @@
+/*
+ * Gerador_funcao.c
+ *
+ * Created: 30/08/2020 15:58:44
+ * Author : Lucas Martins Mendes & Matheus Reibnitz Willemann
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
